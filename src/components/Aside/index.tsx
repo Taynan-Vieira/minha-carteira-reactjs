998bd4bc856e9fdddae590ded65d8aa5b5919460
Taynan-Vieira <taynan.vieira.one@gmail.com>
2020-11-18 15:24:37 -0300

    secao3-aula14-criando o menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,14 +1,53 @@
 import React from 'react';
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp,
+} from 'react-icons/md';
 
-import { Container } from './styles'
+import {
+    Container,
+    Header,
+    LogoImg,
+    MenuContainer,
+    MenuItemLink,
+    Title,
+} from './styles';
+
+
+import logoImg from '../../assets/logo.svg';
 
 const Aside: React.FC = () => {
     return (
         <Container>
-            <h1>Aside</h1>
-        </Container>
+            <Header>
+                <LogoImg src={logoImg} alt="Logo minha carteira" />
+                <Title>Minha Carteira</Title>
+            </Header>
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard/>
+                    Dashboard
+                </MenuItemLink>
 
-    )
+                <MenuItemLink href="#">
+                    <MdArrowUpward/>
+                    Entradas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward/>
+                    Saídas
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdExitToApp/>
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
+        </Container>
+    );
 }
 
 export default Aside;
