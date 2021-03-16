a63d634209c264bd33a7e98a6df604411ef08aa6
Taynan Vieira <taynan.vieira.one@gmail.com>
2021-03-16 16:01:22 -0300

    sessao5-aula-27-filtrando pelo tipo de movimento financeiro

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,25 +1,25 @@
-import React, { useMemo, useState, useEffect } from 'react';
-import { Container, Content, Filters }from './styles';
-import ContentHeader from '../../components/ContentHeader';
-import HistoryFinanceCard from '../../components/HistoryFinanceCard';
-import { uuid } from 'uuidv4';
+import React, { useMemo, useState, useEffect } from "react";
+import { Container, Content, Filters } from "./styles";
+import ContentHeader from "../../components/ContentHeader";
+import HistoryFinanceCard from "../../components/HistoryFinanceCard";
+import { uuid } from "uuidv4";
 
-import SelectInput from '../../components/SelectInput';
-import gains from '../../repositories/gains';
-import expenses from '../../repositories/expenses';
-import formatCurrency from '../../utils/formatCurrenty';
-import formatDate from '../../utils/formatDate';
-import monthsList from '../../utils/months';
+import SelectInput from "../../components/SelectInput";
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
+import formatCurrency from "../../utils/formatCurrenty";
+import formatDate from "../../utils/formatDate";
+import monthsList from "../../utils/months";
 
-interface IRouteParams{
+interface IRouteParams {
   match: {
     params: {
       type: string;
-    }
-  }
+    };
+  };
 }
 
-interface IData{
+interface IData {
   id: string;
   description: string;
   amountFormatted: string;
@@ -28,120 +28,157 @@ interface IData{
   tagColor: string;
 }
 
-const List: React.FC<IRouteParams> = ({ match }) =>{
-  const[data, setData] = useState<IData[]>([]);
-  const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
-  const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear()));
+const List: React.FC<IRouteParams> = ({ match }) => {
+  const [data, setData] = useState<IData[]>([]);
+  const [monthSelected, setMonthSelected] = useState<string>(
+    String(new Date().getMonth() + 1)
+  );
+  const [yearSelected, setYearSelected] = useState<string>(
+    String(new Date().getFullYear())
+  );
+  const [selectedFrequency, setSelectedFrequency] = useState([
+    "recorrente",
+    "eventual",
+  ]);
 
-  
   const { type } = match.params;
 
   const titleProperties = useMemo(() => {
-    return type === 'entry-balance' ? {
-      title: 'Entradas',
-      lineColor: '#F7931B'
-    }
-    : {
-      title: 'Saídas',
-      lineColor: '#E44C4E'
-    };
-  },[type]);
+    return type === "entry-balance"
+      ? {
+          title: "Entradas",
+          lineColor: "#F7931B",
+        }
+      : {
+          title: "Saídas",
+          lineColor: "#E44C4E",
+        };
+  }, [type]);
 
   const listData = useMemo(() => {
-    return type === 'entry-balance' ? gains : expenses;
-  },[type]);
+    return type === "entry-balance" ? gains : expenses;
+  }, [type]);
 
   const months = useMemo(() => {
     return monthsList.map((month, index) => {
       return {
         value: index + 1,
         label: month,
-      }
+      };
     });
-    }, []);
+  }, []);
 
   const years = useMemo(() => {
     let uniqueYears: number[] = [];
 
-    listData.forEach(item => {
+    listData.forEach((item) => {
       const date = new Date(item.date);
       const year = date.getFullYear();
 
-      if(!uniqueYears.includes(year)){
+      if (!uniqueYears.includes(year)) {
         uniqueYears.push(year);
       }
     });
 
-    return uniqueYears.map(year => {
-      return{
+    return uniqueYears.map((year) => {
+      return {
         value: year,
         label: year,
-      }
+      };
     });
   }, [listData]);
 
-    useEffect(() => {
-     const filteredDate = listData.filter(item => {
-       const date = new Date(item.date);
-       const month = String(date.getMonth() + 1);
-       const year = String(date.getFullYear());
+  const handleFrequencyClick = (frequency: string) => {
+    const alreadySelected = selectedFrequency.findIndex(
+      (item) => item === frequency
+    );
 
-       return month === monthSelected && year === yearSelected;
-     });
+    if (alreadySelected >= 0) {
+      const filtered = selectedFrequency.filter((item) => item !== frequency);
+      setSelectedFrequency(filtered);
+    } else {
+      setSelectedFrequency((prev) => [...prev, frequency]);
+    }
+  };
 
-     const formattedData = filteredDate.map(item => {
+  useEffect(() => {
+    const filteredDate = listData.filter((item) => {
+      const date = new Date(item.date);
+      const month = String(date.getMonth() + 1);
+      const year = String(date.getFullYear());
 
-        return{
-          id: uuid(),
-          description: item.description,
-          amountFormatted: formatCurrency(Number(item.amount)),
-          frequency: item.frequency,
-          dataFormatted: formatDate(item.date),
-          tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
-        }
-      })
+      return (
+        month === monthSelected &&
+        year === yearSelected &&
+        selectedFrequency.includes(item.frequency)
+      );
+    });
 
-      setData(formattedData);
-      
-    },[listData, monthSelected, yearSelected, data.length]);
+    const formattedData = filteredDate.map((item) => {
+      return {
+        id: uuid(),
+        description: item.description,
+        amountFormatted: formatCurrency(Number(item.amount)),
+        frequency: item.frequency,
+        dataFormatted: formatDate(item.date),
+        tagColor: item.frequency === "recorrente" ? "#4E41F0" : "#E44C4E",
+      };
+    });
 
-  return(
+    setData(formattedData);
+  }, [listData, monthSelected, yearSelected, data.length, selectedFrequency]);
+
+  return (
     <Container>
-      <ContentHeader title={titleProperties.title} lineColor={titleProperties.lineColor}>
-        <SelectInput options={months} onChange={(e) => setMonthSelected(e.target.value)} defaultValue={monthSelected}/>
-        <SelectInput options={years} onChange={(e) => setYearSelected(e.target.value)} defaultValue={yearSelected}/>
+      <ContentHeader
+        title={titleProperties.title}
+        lineColor={titleProperties.lineColor}
+      >
+        <SelectInput
+          options={months}
+          onChange={(e) => setMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => setYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
 
       <Filters>
-        <button type="button"
-          className="tag-filter tag-filter-recurrent"
+        <button
+          type="button"
+          className={`tag-filter tag-filter-recurrent 
+          ${selectedFrequency.includes("recorrente") && "tag-actived"}`}
+          onClick={() => handleFrequencyClick("recorrente")}
         >
           Recorrentes
         </button>
 
-        <button type="button"
-          className="tag-filter tag-filter-eventual"
+        <button
+          type="button"
+          className={`tag-filter tag-filter-eventual 
+          ${selectedFrequency.includes("eventual") && "tag-actived"}`}
+          onClick={() => handleFrequencyClick("eventual")}
         >
           Eventuais
         </button>
       </Filters>
 
       <Content>
-        {
-          data.map(item => (
-            <HistoryFinanceCard
-              key={item.id}
-              tagColor={item.tagColor}
-              title={item.description}
-              subtitle={item.dataFormatted}
-              amount={item.amountFormatted}
-            />
-          ))
-        }
-        
+        {data.map((item) => (
+          <HistoryFinanceCard
+            key={item.id}
+            tagColor={item.tagColor}
+            title={item.description}
+            subtitle={item.dataFormatted}
+            amount={item.amountFormatted}
+          />
+        ))}
       </Content>
     </Container>
-  )
-}
+  );
+};
 
 export default List;
